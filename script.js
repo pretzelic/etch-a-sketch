5121a7c2e597191d603c88f3b14b03532fbcdf25
Lyubomir <l.mladenov1994@gmail.com>
2019-07-20 10:21:11 +0300

    Color changer and randomizer added

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let main = document.getElementById("main");
-
-function size() {
-    let pixels = document.getElementById('pixels').value;
-    return pixels;
-}
+let styleSheets = document.styleSheets[0];
+let randomColor = false;
+console.log(styleSheets.cssRules[4]);
 
 function clearBoxes(){
     while (main.hasChildNodes()) {
@@ -11,27 +9,58 @@ function clearBoxes(){
     }
 }
 
-function createBoxes(a) {
-    console.log(a, size);
+function createBoxes() {
+    let pixels = Number(document.getElementById('pixels').value);
     clearBoxes();
-    let dimensions = 700/a;
-    for (let i=0; i<a; i++) {
+    let dimensions = 700/pixels;
+    for (let i=0; i<pixels; i++) {
         let rows = document.createElement("div");
         rows.className = "rows";
         main.appendChild(rows);
-        for (let j=0; j<a; j++) {
+        for (let j=0; j<pixels; j++) {
             let box = document.createElement("div");
             box.className = "empty";
             box.addEventListener("mouseover", changeColor);
-            box.style.height = (dimensions-2) + "px";
-            box.style.width = (dimensions-2) + "px";
+            styleSheets.cssRules[2].style.height = (dimensions-2) + "px";
+            styleSheets.cssRules[2].style.width = (dimensions-2) + "px";
             rows.appendChild(box);
         }
     }
 }
 
-function changeColor() {
-    this.style.backgroundColor = "black";
+function reset() {
+    let colored = document.getElementsByClassName("active");
+    colored = Array.prototype.slice.call(colored);
+    colored.forEach(element => {
+        element.classList.remove("active");
+        element.removeAttribute("style");
+    });
 }
 
-createBoxes(30);
+function random() {
+    if (!randomColor) {
+        randomColor = true;
+    }else {
+        randomColor = false;
+    } 
+}
+
+function randomizeColor() {
+    let reds = Math.ceil(Math.random()*255);
+    let greens = Math.ceil(Math.random()*255);
+    let blues = Math.ceil(Math.random()*255);
+    return `rgb(${reds},${greens},${blues})`
+}
+
+function changeColor() {
+    if(!randomColor){
+        let color = document.getElementById('color').value;
+        this.style.backgroundColor = color;
+        this.classList.add("active");
+    }else{
+        this.style.backgroundColor = randomizeColor();
+        this.classList.add("active");
+    }
+}
+
+createBoxes();
